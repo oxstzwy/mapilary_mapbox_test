03d841c8e93bde69fe8890f28d16cb4581459009
oxstzwy <140463222+oxstzwy@users.noreply.github.com>
2025-01-19 11:49:27 -0400

    Update Index.jsx

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -5,8 +5,8 @@ const Index = () => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
       <div className="text-center mb-8">
-        <h1 className="text-4xl font-bold mb-4">Bem vindos ao Teste utilizando Mapillary e Mapbox </h1>
-        <p className="text-xl text-gray-600">Testes executados no âmbito do Campus Map UFAM</p>
+        <h1 className="text-4xl font-bold mb-4">Bem vindos ao Teste utilizando Mapillary e Mapbox de Matheus</h1>
+        <p className="text-xl text-gray-600">Testes executados no âmbito do Campus Map UFAM e FAEXP</p>
       </div>
       <Link
         to="/map"
